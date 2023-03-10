70b1223aaa5caeb9600d2d00082eebf6c60e93fe
Garvit1809 <garvitvarshne18@gmail.com>
2023-03-10 11:16:58 +0530

    adding APiFeatures Paginate, sort n filter

--- controllers/handlerFactory.js
+++ controllers/handlerFactory.js
@@ -1,6 +1,6 @@
-const catchAsync = require("../Utils/catchAsync");
-const AppError = require("../Utils/appError");
-const APIFeatures = require("../Utils/apiFeatures");
+const catchAsync = require("../utils/catchAsync");
+const AppError = require("../utils/appError");
+const APIFeatures = require("../utils/apiFeatures");
 
 exports.deleteOne = (Model) =>
   catchAsync(async (req, res, next) => {
@@ -65,25 +65,24 @@ exports.getOne = (Model, popOptions) =>
     });
   });
 
-  exports.getAll = Model =>
+exports.getAll = (Model) =>
   catchAsync(async (req, res, next) => {
     // To allow for nested GET reviews on tour (hack)
     let filter = {};
-    if (req.params.tourId) filter = { tour: req.params.tourId };
+    // if (req.params.tourId) filter = { tour: req.params.tourId };
 
     const features = new APIFeatures(Model.find(filter), req.query)
       .filter()
       .sort()
       .limitFields()
       .paginate();
-    // const doc = await features.query.explain();
     const doc = await features.query;
 
     res.status(200).json({
-      status: 'success',
+      status: "success",
       results: doc.length,
       data: {
-        data: doc
-      }
+        data: doc,
+      },
     });
   });
