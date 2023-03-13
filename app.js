4ffac54815812c0b26cd06e0df751a22dc97af2f
Garvit1809 <garvitvarshne18@gmail.com>
2023-03-13 18:25:04 +0530

    adding error handling routes

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
-const express = require('express');
-
+const express = require("express");
+const AppError = require("./utils/appError");
+const globalErrorHandler = require('./controllers/errorController');
 const app = express();
 
-app.use(express.json({ limit: '10kb' }));
+app.use(express.json({ limit: "10kb" }));
 
-app.get('/', (req,res,next) => {
-    console.log(req.protocol);
-    res.send('hello')
-})
+app.all("*", (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
+
+app.use(globalErrorHandler);
 
 module.exports = app;
 
