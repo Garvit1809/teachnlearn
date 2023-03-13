2da74a2e825719d776a117967c2bbc32633777ef
Garvit1809 <garvitvarshne18@gmail.com>
2023-03-13 09:29:29 +0530

    adding copy.env file

--- app.js
+++ app.js
@@ -4,4 +4,18 @@ const app = express();
 
 app.use(express.json({ limit: '10kb' }));
 
+app.get('/', (req,res,next) => {
+    console.log(req.protocol);
+    res.send('hello')
+})
+
 module.exports = app;
+
+// auth complete
+// teach card and learning card APIs
+// Permanent Gig/Cards
+// Specific Cards
+// Classroom API
+// review Model, APIS
+
+// forum API
