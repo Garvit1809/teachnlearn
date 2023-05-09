b05cc35d355d1e24d23265d1772ea14e4c45ff58
Garvit1809 <garvitvarshne18@gmail.com>
2023-05-10 00:36:10 +0530

    adding multistepform hook

---
+++ client/src/components/authentication-comp/descriptionBox.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import styled from "styled-components";
+import { DoubleQuoteIcon } from "../general-components/svg";
+
+const Section = styled.div`
+  background-color: #0d1c2e;
+  border-radius: 24px;
+
+  width: calc(50vw - 4rem);
+  /* height: 100%; */
+  /* height: calc(100vh - 4rem); */
+
+  h4 {
+    font-weight: 600;
+    font-size: 34px;
+    line-height: 60px;
+    color: #ffffff;
+  }
+
+  p {
+    color: #ffffff;
+    font-weight: 600;
+    font-size: 44px;
+    line-height: 60px;
+  }
+
+  span {
+    color: #ffffff;
+    font-weight: 400;
+    font-size: 20px;
+    line-height: 27px;
+  }
+`;
+
+const DescriptionBox = () => {
+  return (
+    <Section>
+      <h4>Get Started</h4>
+      <DoubleQuoteIcon />
+      <p>If you want to master something, teach it.</p>
+      <span>- Richard Feynman</span>
+    </Section>
+  );
+};
+
+export default DescriptionBox;
--- client/src/pages/authentication/newSignup.tsx
+++ client/src/pages/authentication/newSignup.tsx
@@ -1,16 +1,22 @@
 import React from 'react'
 import styled from 'styled-components'
+import DescriptionBox from '../../components/authentication-comp/descriptionBox'
 
 const Section = styled.div`
     display: flex;
     align-items: center;
     justify-content: center;
+    padding: 2rem;
+    box-sizing: border-box;
 `
 
 const NewSignup = () => {
   return (
     <Section>
-      
+      <form >
+
+      </form>
+      <DescriptionBox />
     </Section>
   )
 }
