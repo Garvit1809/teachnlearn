f887f4cf691e7d77155990738fc991e1cb8eae37
Garvit1809 <garvitvarshne18@gmail.com>
2023-07-25 17:50:10 +0530

    adding new server link digitalocean

--- client/src/utils/apiRoutes.ts
+++ client/src/utils/apiRoutes.ts
@@ -1,6 +1,7 @@
 // export const BASE_URL = "http://localhost:8000";
 // export const BASE_URL = "https://teachnlearn-server-test-deploy.vercel.app";
-export const BASE_URL = "http://165.22.218.141:8000";
+// export const BASE_URL = "http://165.22.218.141:8000";
+export const BASE_URL = "https://coral-app-ndhml.ondigitalocean.app";
 
 export const apiVersion = "/api/v1";
 // export const userEnd = "api/v1/users/"
