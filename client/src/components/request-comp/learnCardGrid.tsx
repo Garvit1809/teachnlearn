a46c53c037037267e0f9ef53827b275da8e5b40f
Garvit1809 <garvitvarshne18@gmail.com>
2023-07-27 14:02:49 +0530

    making requests page responsive

--- client/src/components/request-comp/learnCardGrid.tsx
+++ client/src/components/request-comp/learnCardGrid.tsx
@@ -8,6 +8,23 @@ const CardGrid = styled.div`
   grid-template-columns: 1fr 1fr 1fr;
   column-gap: 2rem;
   row-gap: 2.5rem;
+
+  @media only screen and (max-width: 1100px) {
+    column-gap: 1rem;
+  }
+  @media only screen and (max-width: 1000px) {
+    grid-template-columns: 1fr 1fr;
+    column-gap: 1.6rem;
+    row-gap: 1.6rem;
+  }
+  @media only screen and (max-width: 750px) {
+    column-gap: 1.5rem;
+    row-gap: 1.5rem;
+  }
+  @media only screen and (max-width: 650px) {
+    grid-template-columns: 1fr;
+    row-gap: 1.5rem;
+  }
 `;
 
 interface learnCardGridProps {
